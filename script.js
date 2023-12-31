d05692a8b631400502d987a51f3163b677582748
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2023-12-31 20:40:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
  function dom() {
     // Code to execute when the DOM is fully loaded
+	  var body = document.body;
+	  if (body.childElementCount === 0) {
     var message = document.createElement('p');
     message.textContent = 'DOM load success';
-	   document.body.appendChild(message);
+	   body.appendChild(message);
+	  }
  }
+
 document.addEventListener('DOMContentLoaded', dom);
 	 
