99b9e4aa1a9d526ed68b2935899c4dec82fa941c
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-06 15:36:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
  function onDOMLoad() {
-      // Get the body element of the document
-     
-      // Create a new paragraph element
-      
       var textNode = document.createTextNode('DOM load success');
 	 document.body.appendChild(textNode);
-    
-     
     }
-  // document.addEventListener('DOMContentLoaded', onDOMLoad());
-    // Event listener for DOMContentLoaded event
-    document.addEventListener('DOMContentLoaded', onDOMLoad);
+ 
+    document.addEventListener('DOMContentLoaded', onDOMLoad());
