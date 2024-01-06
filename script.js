a3bc548be6bd0136e36570116407417b063a471f
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-06 15:34:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,14 +2,12 @@
       // Get the body element of the document
      
       // Create a new paragraph element
-      var paragraph = document.createElement('p');
-
-      // Set the text content of the paragraph
-      paragraph.textContent = "DOM load success";
-
-      // Append the paragraph to the body
+      
+      var textNode = document.createTextNode('DOM load success');
+	 document.body.appendChild(textNode);
+    
      
     }
-
+  // document.addEventListener('DOMContentLoaded', onDOMLoad());
     // Event listener for DOMContentLoaded event
     document.addEventListener('DOMContentLoaded', onDOMLoad);
